241a9c547a90788141f17288ef45df0fd68e1274
Alexis ALGOUD <alexis@atm-consulting.fr>
2016-09-26 10:49:42 +0200

    TODO sur hook

--- class/actions_alertstockwarehouse.class.php
+++ class/actions_alertstockwarehouse.class.php
@@ -66,7 +66,7 @@ class ActionsAlertStockWarehouse
 		if (in_array('stockproductcard', explode(':', $parameters['context'])))
 		{
 			
-			$fk_product = GETPOST('id', 'int');
+			$fk_product = GETPOST('id', 'int'); //TODO AA a priori déjà présent dans $object->id, n'utiliser GETPOST dans un hook qu'en dernier recours
 			
 			define('INC_FROM_DOLIBARR',true);
 		
@@ -79,9 +79,10 @@ class ActionsAlertStockWarehouse
 			if (strpos($action ,  'setlimite_') === 0)
 			{
 				$stocklimit = GETPOST(substr($action, 3), 'int');
-				$TRes = explode('_',$action);
+				$TRes = explode('_',$action); //TODO ne serait-ce pas redondant avec $stocklimit
+				// $stocklimit = $TRes[1] non ?
 				
-				$stock = new TStock;
+				$stock = new TStock; //TODO TStock trop generique => TAlertStockWarehouse
 				
 				$stock->fetch($TRes[1], $fk_product);
 				
@@ -91,20 +92,22 @@ class ActionsAlertStockWarehouse
    				$stock->fk_entrepot = $TRes[1];
 				$stock->limite = $stocklimit;
 				//
-				if($stocklimit == NULL){
+				if($stocklimit == NULL){ // TODO ===
 					$stock->delete($PDOdb);
 				}else {
 				
 					$stock->save($PDOdb);
 				}
+				//TODO tu peux aussi écrire $stock->to_delete = true; $stock->save($PDOdb);
 				
-				 if ($result < 0)
+				 if ($result < 0) //TODO { } sinon sur une seule ligne
 				    setEventMessages($object->error, $object->errors, 'errors');
 				  
 				$action='';
 			}
 			
 			//Récupération des données
+			//TODO préfère l'écriture avec des sauts de lignes dans la chaine $sql plutôt que la concaténation
 			$sql = "SELECT e.rowid, e.label, e.entity, abs.fk_product, abs.limite";
 			$sql.= " FROM ".MAIN_DB_PREFIX."entrepot as e";
 			$sql.= ' LEFT JOIN '.MAIN_DB_PREFIX.'alert_by_stock as abs ON (abs.fk_entrepot = e.rowid AND abs.fk_product = '.(int) $fk_product.')';
@@ -115,7 +118,7 @@ class ActionsAlertStockWarehouse
 			if ($resql && $db->num_rows($resql) > 0)
 			{
 				$form = new Form($db);
-				while ($obj = $db->fetch_object($resql))
+				while ($obj = $db->fetch_object($resql)) //TODO si tu n'as pas besoin du nb de tuple, tu peux ne garder que le while
 				{
 					print '<tr><td>'.$form->editfieldkey("Seuil limite d'alerte pour entrepot ".$obj->label,'limite_'.$obj->rowid.'',$obj->limite,$object,$user->rights->produit->creer).'</td><td colspan="2">';
 				    print $form->editfieldval("Seuil limite d'alerte pour entrepot ".$obj->label,'limite_'.$obj->rowid.'',$obj->limite,$object,$user->rights->produit->creer,'string');
